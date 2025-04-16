dd403e3d4cafcadb4a9bc4cf13604a32f8198671
DanielaBiolek <daniela.biolek@centrum.cz>
2025-04-16 14:24:19 +0200

    asi hotov hamburger

--- script.js
+++ script.js
@@ -8,10 +8,54 @@ hamMenu.addEventListener('click', () => {
     }
 )
 
-const menuItem = document.querySelector('.menu_item-ham');
+const menuItem1 = document.querySelector('#ham-item1');
 
-menuItem.addEventListener('click', () => {
+menuItem1.addEventListener('click', () => {
     hamMenu.classList.toggle('active'); 
     offScreenMenu.classList.toggle('active');
+    }
+)
+
+const menuItem2 = document.querySelector('#ham-item2');
+
+menuItem2.addEventListener('click', () => {
+    hamMenu.classList.toggle('active'); 
+    offScreenMenu.classList.toggle('active');
+    }
+)
+const menuItem3 = document.querySelector('#ham-item3');
+
+menuItem3.addEventListener('click', () => {
+    hamMenu.classList.toggle('active'); 
+    offScreenMenu.classList.toggle('active');
+    }
+)
+const menuItem4 = document.querySelector('#ham-item4');
+
+menuItem4.addEventListener('click', () => {
+    hamMenu.classList.toggle('active'); 
+    offScreenMenu.classList.toggle('active');
+    }
+)
+const menuItem5 = document.querySelector('#ham-item5');
+
+menuItem5.addEventListener('click', () => {
+    hamMenu.classList.toggle('active'); 
+    offScreenMenu.classList.toggle('active');
+    }
+)
+
+const hamburger = document.querySelector('.menu_hamburger');
+const span1 = document.querySelector('#hamspan1');
+const span2 = document.querySelector('#hamspan2');
+const span3 = document.querySelector('#hamspan3');
+
+hamburger.addEventListener('mouseover', () => {
+    hamburger.classList.add('hover')
+    }
+)
+
+hamburger.addEventListener('mouseout', () => {
+    hamburger.classList.remove('hover')
     }
 )
